f0326b6cb500f6ac64b9205ec0bb83fdb91f95bf
RaafaNabilRabbani <raafanabilrabbani@gmail.com>
2026-05-29 02:13:32 +0700

    tambah perubahan button saat di click

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Filter Videos
-    const filterButtons = document.querySelectorAll('.filter-btn');
+    const filterButtons = document.querySelectorAll('.btn-filter');
     const videoItems = document.querySelectorAll('.video-item');
 
     filterButtons.forEach(button => {
         button.addEventListener('click', () => {
+            const filter = button.getAttribute('data-filter');
+            
+            filterButtons.forEach(btn => btn.classList.remove('active'));
+            button.classList.add('active');
+
             const filter = button.getAttribute('data-filter');
 
             videoItems.forEach(item => {
